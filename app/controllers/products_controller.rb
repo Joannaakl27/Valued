3b93380c7469605487cf8e8fb78282a5b3e90938
joanna akl <akljoanna95@gmail.com>
2022-03-15 20:48:13 +0100

    routing of the show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,11 @@ class ProductsController < ApplicationController
     else
      @products = Product.all
     end
+  end
 
+  def show
+    response = URI.open("https://ch-en.openfoodfacts.org/api/v0/product/#{params[:id]}.json")
+    json = JSON.parse(response.read)
+    @product = json['product']
   end
 end
