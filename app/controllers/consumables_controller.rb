2bb40695785737bdf470306d2680447519302ef6
joanna akl <akljoanna95@gmail.com>
2022-03-22 22:14:51 +0100

    created consumables logic + view

---
+++ app/controllers/consumables_controller.rb
@@ -0,0 +1,26 @@
+class ConsumablesController < ApplicationController
+  def new
+    @consumable = Consumable.new
+    response = URI.open("https://ch-en.openfoodfacts.org/api/v0/product/#{params[:id]}.json")
+    json = JSON.parse(response.read)
+    @product = json['product']
+  end
+
+  def create
+    @consumable = Consumable..new(consumable_params)
+    response = URI.open("https://ch-en.openfoodfacts.org/api/v0/product/#{params[:id]}.json")
+    json = JSON.parse(response.read)
+    @product = json['product']
+    @consumable.user = current_user
+    @consumable.product_barcode = @product["_id"]
+    @consumable.product_name = @product["product_name"]
+    @consumable.product_image = @product["image_front_url"]
+    @consumable.product_brand = @product["brands"]
+  end
+
+  private
+
+  def consumable_params
+    params.require(:consumable).permit(:serving_size, :number_of_servings, :purchase_date)
+  end
+end
